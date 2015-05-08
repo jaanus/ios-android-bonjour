eb3298c4088d81c4b3e16781581ab2422294229f
jaanus <jaanus@gmail.com>
2015-05-08 16:45:31 +0300

    Basic service browsing works.

--- osx/BonjourTool/BonjourTool/WindowController.swift
+++ osx/BonjourTool/BonjourTool/WindowController.swift
@@ -8,12 +8,49 @@
 
 import Cocoa
 
-class WindowController: NSWindowController {
+class WindowController: NSWindowController, NSNetServiceBrowserDelegate, NSNetServiceDelegate {
 
+    let serviceBrowser = NSNetServiceBrowser()
+    var netServices: Array<NSNetService> = []
+    
     override func windowDidLoad() {
         super.windowDidLoad()
     
+        serviceBrowser.delegate = self
+        serviceBrowser.searchForServicesOfType("_jktest._tcp", inDomain: "")
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
-
+    
+    
+    
+    // MARK: - NSNetServiceBrowserDelegate
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didFindService aNetService: NSNetService, moreComing: Bool) {
+        println("service browser found service: \(aNetService). more coming: \(moreComing)")
+        
+        if !contains(netServices, aNetService) {
+            netServices.append(aNetService)
+        }
+        
+        aNetService.delegate = self
+        aNetService.resolveWithTimeout(5)
+    }
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didRemoveService aNetService: NSNetService, moreComing: Bool) {
+        println("service browser removed service: \(aNetService). more coming: \(moreComing)")
+        if let i = find(netServices, aNetService) {
+            netServices.removeAtIndex(i)
+        }
+    }
+    
+    
+    // MARK: - NSNetServiceDelegate
+    
+    func netServiceDidResolveAddress(sender: NSNetService) {
+        println("net service did resolve address: \(sender)")
+    }
+    
+    func netService(sender: NSNetService, didNotResolve errorDict: [NSObject : AnyObject]) {
+        println("service \(sender) did not resolve. error: \(errorDict)")
+    }    
 }
