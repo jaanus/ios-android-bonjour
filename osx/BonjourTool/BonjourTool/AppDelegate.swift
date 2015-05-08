50e027feb74485d9d88c563e519eb968440dc345
jaanus <jaanus@gmail.com>
2015-05-08 15:38:58 +0300

    basic tool scaffolding

---
+++ osx/BonjourTool/BonjourTool/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BonjourTool
+//
+//  Created by Jaanus Kase on 08.05.15.
+//  Copyright (c) 2015 Jaanus Kase. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
