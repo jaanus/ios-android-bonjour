3f306dcdc6d94143b223f2572dab7a7f46f2f949
jaanus <jaanus@gmail.com>
2015-05-08 15:45:00 +0300

    iOS provider.

---
+++ osx/BonjourTool/iosProvider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosProvider
+//
+//  Created by Jaanus Kase on 08.05.15.
+//  Copyright (c) 2015 Jaanus Kase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
