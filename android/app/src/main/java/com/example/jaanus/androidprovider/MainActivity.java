ea2aac56fdd68f05231d016966d338cd27fd4c6e
jaanus <jaanus@gmail.com>
2015-05-10 11:07:57 +0300

    Better Android lifecycle

--- android/app/src/main/java/com/example/jaanus/androidprovider/MainActivity.java
+++ android/app/src/main/java/com/example/jaanus/androidprovider/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.Inet4Address;
@@ -29,36 +30,8 @@ public class MainActivity extends ActionBarActivity {
     private int PORT;
     private MyHTTPD server;
     private NsdServiceInfo serviceInfo;
-    private NsdManager mNsdManager;
-
-    private NsdManager.RegistrationListener mRegistrationListener = new NsdManager.RegistrationListener() {
-
-        @Override
-        public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
-            // Save the service name.  Android may have changed it in order to
-            // resolve a conflict, so update the name you initially requested
-            // with the name Android actually used.
-            String mServiceName = NsdServiceInfo.getServiceName();
-            Log.d("WAT", "Registered service");
-        }
-
-        @Override
-        public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
-            // Registration failed!  Put debugging code here to determine why.
-            Log.d("WAT", "Failed to register service");
-        }
-
-        @Override
-        public void onServiceUnregistered(NsdServiceInfo arg0) {
-            // Service has been unregistered.  This only happens when you call
-            // NsdManager.unregisterService() and pass in this listener.
-        }
-
-        @Override
-        public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
-            // Unregistration failed.  Put debugging code here to determine why.
-        }
-    };
+    private static final String TAG = "AndroidProvider";
+    private NsdManager.RegistrationListener mRegistrationListener;
 
 
     @Override
@@ -66,36 +39,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Integer a = 3 + 4;
-        Log.d("WAT", "Message " + Integer.toString(a));
-        Log.d("WAT", "My ip address is " + getLocalIpAddress());
-
-        ServerSocket mServerSocket = null;
-        try {
-            mServerSocket = new ServerSocket(0);
-            // Store the chosen port.
-            PORT = mServerSocket.getLocalPort();
-            mServerSocket.close();
-
-            serviceInfo  = new NsdServiceInfo();
-
-            // The name is subject to change based on conflicts
-            // with other services advertised on the same network.
-            Log.d("WAT", "port is " + Integer.toString(PORT));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // register the bonjour service
-        NsdServiceInfo serviceInfo  = new NsdServiceInfo();
-        serviceInfo.setServiceName("jktestandroid");
-        serviceInfo.setServiceType("_jktest._tcp.");
-        serviceInfo.setPort(PORT);
-
-        mNsdManager = (NsdManager)this.getApplicationContext().getSystemService(Context.NSD_SERVICE);
-
-        mNsdManager.registerService(
-                serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
     }
 
     @Override
@@ -122,30 +65,103 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "onResume");
         super.onResume();
+        this.startServer();
+    }
 
-        Log.d("WAT", "Attempting to start myHTTPD");
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+        this.stopServer();
+    }
 
-//        TextView textIpaddr = (TextView) findViewById(R.id.ipaddr);
-//        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-//        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
-//        final String formatedIpAddress = String.format("%d.%d.%d.%d", (ipAddress & 0xff), (ipAddress >> 8 & 0xff),
-//                (ipAddress >> 16 & 0xff), (ipAddress >> 24 & 0xff));
-//        textIpaddr.setText("Please access! http://" + formatedIpAddress + ":" + PORT);
+    private void startServer() {
 
+        // create the registration listener who will be the callback object for service registration
+        mRegistrationListener = new NsdManager.RegistrationListener() {
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
+                // Save the service name.  Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                String mServiceName = NsdServiceInfo.getServiceName();
+                Log.d(TAG, "Registered service. Actual name used: " + mServiceName);
+            }
+
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed!  Put debugging code here to determine why.
+                Log.d(TAG, "Failed to register service");
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo arg0) {
+                // Service has been unregistered.  This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+                Log.d(TAG, "Unregistered service");
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                Log.d(TAG, "Service unregistration failed");
+                // Unregistration failed.  Put debugging code here to determine why.
+            }
+        };
+
+        // obtain a free port number by binding to a socket and then unbinding again so the port is available to us
+        // nicer would be to let NanoHTTPD do this and just query it for the port
+
+        ServerSocket mServerSocket = null;
+        try {
+            mServerSocket = new ServerSocket(0);
+            // Store the chosen port.
+            PORT = mServerSocket.getLocalPort();
+            mServerSocket.close();
+
+            // The name is subject to change based on conflicts
+            // with other services advertised on the same network.
+            Log.d(TAG, "port is " + Integer.toString(PORT));
+            Log.d(TAG, "ip is " + this.getLocalIpAddress());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // run the server
         try {
             server = new MyHTTPD();
             server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // after server is started, register the bonjour service
+        serviceInfo  = new NsdServiceInfo();
+        serviceInfo.setServiceName("jktest_androidprovider");
+        serviceInfo.setServiceType("_jktest._tcp.");
+        serviceInfo.setPort(PORT);
+
+        NsdManager mNsdManager = (NsdManager)this.getApplicationContext().getSystemService(Context.NSD_SERVICE);
+
+        mNsdManager.registerService(
+                serviceInfo, NsdManager.PROTOCOL_DNS_SD, mRegistrationListener);
+
+        // Report the address to UI
+        TextView urlView = (TextView)findViewById(R.id.url);
+        urlView.setText("Now listening on http://" + getLocalIpAddress() + ":" + Integer.toString(PORT) + "/");
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (server != null)
+    private void stopServer() {
+        // unregister the service
+        NsdManager mNsdManager = (NsdManager)this.getApplicationContext().getSystemService(Context.NSD_SERVICE);
+        mNsdManager.unregisterService(mRegistrationListener);
+
+        // stop server
+        if (server != null) {
             server.stop();
+        }
     }
 
     public String getLocalIpAddress() {
@@ -155,12 +171,14 @@ public class MainActivity extends ActionBarActivity {
                 for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
                     InetAddress inetAddress = enumIpAddr.nextElement();
                     if ((inetAddress instanceof Inet4Address) && !inetAddress.isLoopbackAddress()) {
+                        // I don’t know how NanoHTTPD, bonjour etc feel about ipv6 addresses
+                        // So to be on the safe side, we filter to ipv4 only
                         return inetAddress.getHostAddress();
                     }
                 }
             }
         } catch (SocketException ex) {
-            Log.e("WAT LOG", ex.toString());
+            Log.e(TAG, ex.toString());
         }
         return null;
     }
@@ -173,18 +191,8 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public Response serve(String uri, Method method, Map<String, String> headers, Map<String, String> parms,
                               Map<String, String> files) {
-//            final StringBuilder buf = new StringBuilder();
-//            for (Map.Entry<String, String> kv : headers.entrySet())
-//                buf.append(kv.getKey()).append(" : ").append(kv.getValue()).append("\n");
-//            handler.post(new Runnable() {
-//                @Override
-//                public void run() {
-////                    hello.setText(buf);
-//                    Log.d("WAT", "I don’t know wtf this is: " + buf);
-//                }
-//            });
 
-            final String html = "Hello, world, from Android. Running at http://" + getLocalIpAddress();
+            final String html = "Hello, world, from Android. Running at http://" + getLocalIpAddress() + ":" + PORT;
             return new NanoHTTPD.Response(html);
         }
     }
